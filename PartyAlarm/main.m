e886b857331ed1f2be89c566b0cd5c3c4cf4db5a
Nikos Kofinas <nikofinas@gmail.com>
2014-01-25 03:55:53 +0200

    first commit

---
+++ PartyAlarm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PartyAlarm
+//
+//  Created by Nikos Kofinas on 1/25/14.
+//  Copyright (c) 2014 Nikos Kofinas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
